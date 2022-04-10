0809fa3242c9d9d7b7de27fe2d22c41d25090ec5
Filipe Garrido <figarrido95@gmail.com>
2022-04-10 00:20:57 -0300

    Adding request from Riot API

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/summoner/:summonerName')
+  getSummonersName() {
+    return this.appService.getSummonersName();
+  }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { get } from 'http';
+import { url } from 'inspector';
+
+const LOL_KEY = 'RGAPI-8da54502-aaca-4955-8dd6-4587b481e454'
+const LOL_URL = 'http://br1.api.riotgames.com'
+const LOL_Icons = 'http://ddragon.leagueoflegends.com/cdn/12.6.1/img/profileicon/'
+
+require('dotenv').config()
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Batata';
+  }
+
+  getSummonersName(): any{
+    return 'Abacate';
   }
 }
